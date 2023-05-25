2bccf3f1ff1af5aff73c682f2ac9dabaaa2f87c8
mikhalkevich <mikhalkevich@ya.ru>
2023-05-25 19:17:52 +0300

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<x-admin-layout>
+    <div class="container grid px-6 mx-auto">
+        <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+            Белорусская косметика
+        </h2>
+    </div>
+    <div class="py-12">
+        <h1 class="text-center font-semibold text-gray-700 dark:text-gray-200">
+           Admin IT Grade
+        </h1>
+    </div>
+</x-admin-layout>
